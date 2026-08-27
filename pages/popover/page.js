ed8e2680d46d7e6a5eb36ff0f2fa6c666da262cf
jiangqiao <miniphper@gmail.com>
2026-08-27 18:32:51 +0800

    优化

--- pages/popover/page.js
+++ pages/popover/page.js
@@ -1,4 +1,8 @@
 Page({
-  data: { open: false },
-  onOpen() { this.setData({ open: true }); }
+  data: { below: false, above: false, aside: false, light: false, nomask: false },
+  onBelow() { this.setData({ below: true, above: false, aside: false, light: false, nomask: false }); },
+  onAbove() { this.setData({ above: true, below: false, aside: false, light: false, nomask: false }); },
+  onRight() { this.setData({ aside: true, below: false, above: false, light: false, nomask: false }); },
+  onLight() { this.setData({ light: true, below: false, above: false, aside: false, nomask: false }); },
+  onNomask() { this.setData({ nomask: !this.data.nomask, below: false, above: false, aside: false, light: false }); }
 });
