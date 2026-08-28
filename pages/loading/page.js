dc3bc306de81b1eefa34591a4b8c12386d77eb60
jiangqiao <miniphper@gmail.com>
2026-08-28 12:09:08 +0800

    优化

--- pages/loading/page.js
+++ pages/loading/page.js
@@ -1,20 +1,7 @@
 Page({
-  data: {
-    loading: true,
-    done: false,
-    spinOnly: true
-  },
-  onStart() {
-    this.setData({ loading: true, done: false, spinOnly: true });
-  },
-  onDone() {
-    this.setData({ loading: false, done: true, spinOnly: false });
-  },
   onMask() {
     this.showLoading('加载中');
-    var that = this;
-    setTimeout(function () {
-      that.hideLoading();
-    }, 1500);
+    System.sleep(1600);
+    this.hideLoading();
   }
 });
